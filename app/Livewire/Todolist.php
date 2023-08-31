c6620740707e1c4ed327d9fa5849ec9cf1e1694e
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-08-31 22:31:57 +0800

    added todo-list app

--- app/Livewire/Todolist.php
+++ app/Livewire/Todolist.php
@@ -1,13 +1,56 @@
 <?php
 
 namespace App\Livewire;
-
+use App\Models\TodoItem;
 use Livewire\Component;
 
 class Todolist extends Component
 {
+    public $todos;
+    public string $todoText = '';
+
+    public function mount(){
+        $this->selectTodos();
+    }
     public function render()
     {
         return view('livewire.todolist');
     }
+   
+    public function addTodo(){
+        $todo = new TodoItem();
+        $todo->name = $this->todoText;
+        $todo->completed = false;
+        $todo->save();
+
+        $this->todoText = '';
+        $this->selectTodos();
+    }
+    public function toggleTodo($id)
+    {
+        $todo = TodoItem::where('id', $id)->first();
+        if (!$todo) {
+            return;
+        }
+        $todo->completed = !$todo->completed;
+        $todo->save();
+        $this->selectTodos();
+    }
+
+    public function deleteTodo($id)
+    {
+        $todo = TodoItem::where('id', $id)->first();
+        if (!$todo) {
+            return;
+        }
+        $todo->delete();
+        $this->selectTodos();
+    }
+    public function selectTodos(){
+        //$this->todo = TodoItem::orderBy('created_at','ASC')->get();
+        $this->todos = TodoItem::orderBy('created_at','ASC')->get();
+    }
+
+
+ 
 }
--- resources/views/livewire/todolist.blade.php
+++ resources/views/livewire/todolist.blade.php
@@ -1,3 +1,22 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<div class="grid place-items-center h-screen">
+    <div class="flex flex-row gap-5">
+        <input type="text" wire:model="todoText" placeholder="Type and hit enter">
+        <button class="bg-indigo-500 hover:bg-indigo-600 rounded w-[100px] text-white" wire:click="addTodo()">
+            Add
+        </button>
+    </div>
+    <div>
+        @if (count($todos) == 0)
+            <p class="text-gray-600">Empty</p>
+        @endif
+        @foreach ($todos as $todo)
+            <div>
+                <div class="flex flex-row items-center gap-5">
+                    <input {{$todo->completed ? 'checked' : ''}} type="checkbox" wire:change="toggleTodo({{$todo->id}})">
+                    <p class="{{$todo->completed ? 'line-through' : ''}}">{{$todo->name}}</p>
+                    <button wire:click="deleteTodo({{$todo->id}})">Delete</button>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
