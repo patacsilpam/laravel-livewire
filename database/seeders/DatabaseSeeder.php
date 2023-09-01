c24141d59564e97a2340bef7b74c1efcf518d09c
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-09-01 22:51:48 +0800

    migrations and factories for countries and continent table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Country;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,11 +12,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         //\App\Models\Continent::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $continents = [
+            ['id'=> 1, 'name' => 'Europe'],
+            ['id' => 2, 'name' => 'Asia'],
+            ['id' => 3, 'name' => 'Africa'],
+            ['id' => 4, 'name' => 'South America'],
+            ['id' => 5, 'name' => 'North America'],
+            ['id' => 6, 'name' => 'Australia'],
+            ['id' => 7, 'name' => 'Antartica']
+        ];
+
+        foreach($continents as $continent){
+            \App\Models\Continent::factory()->create($continent)
+                ->each(function($c){
+                    $c->countries()->saveMany(Country::factory(10)->make());
+                });
+        }
     }
 }
